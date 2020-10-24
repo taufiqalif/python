028c93b048e479c7a7352b6e18fd51a9e7b38640
taufiqalif <aliftaufiq4@gmail.com>
2020-10-24 23:02:24 +0700

    menghapus dan menambahkan yang baru

--- biodata.py
+++ biodata.py
@@ -8,8 +8,11 @@ This is a temporary script file.
 nama = input ("masukan nama: ")
 nama2 = input ("masukan nama panggilan: ")
 tgl = input ("masukan tanggal lahir: ")
-bulan = input ("masukan bulan lahir: ")
+kota = input ("kota lahir kamu: ")
+npm = input ("masukan no mahasiswa anda: ")
+no = input ("no hp kamu: ")
+umr = input("umur anda sekarang: ")
 
 #output
 
-print ("nama lengkat saya",nama,"nama panggilan saya",nama2,"tanggal lahir saya",tgl,"saya lahir bulan",bulan,)
+print ("nama lengkat saya",nama,", nama panggilan saya",nama2,"tanggal lahir saya",tgl,"saya lahir di kota",kota,", dan umur saya sekarang adalah",umr,"tahun, NIM saya adalah",npm,",kalian bisa hubungi saya meelalui no ini",no,)
