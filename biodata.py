f98fc80ca710c32a3882ff362ba2b71ed5ac5391
taufiqalif <aliftaufiq4@gmail.com>
2020-10-24 10:25:20 +0700

    file baru

---
+++ biodata.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+nama = input ("masukan nama: ")
+nama2 = input ("masukan nama panggilan: ")
+tgl = input ("masukan tanggal lahir: ")
+bulan = input ("masukan bulan lahir: ")
+
+#output
+
+print ("nama lengkat saya",nama,"nama panggilan saya",nama2,"tanggal lahir saya",tgl,"saya lahir bulan",bulan,)
